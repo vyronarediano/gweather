a210eb17bc4ec507d3cd6217a1b83150e28fce49
Vyron Arediano <vyron.arediano@cormanttech.com>
2022-07-11 12:56:39 +0800

    improve: Reduce view pager sensisitivity so that it will not conflict the refresh layout - improves UX

--- app/src/main/java/com/ced/gweather/weather/ui/weatherhome/WeatherHomeFragment.kt
+++ app/src/main/java/com/ced/gweather/weather/ui/weatherhome/WeatherHomeFragment.kt
@@ -3,7 +3,9 @@ package com.ced.gweather.weather.ui.weatherhome
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.ced.authentication.domain.model.User
 import com.ced.commons.ui.extensions.viewModel
 import com.ced.commons.ui.observe
@@ -40,6 +42,19 @@ class WeatherHomeFragment: BaseFragmentDI() {
         weatherHomeViewModel.loadLoggedInUser()
 
         setupTabs()
+
+        pager.reduceDragSensitivity()
+    }
+
+    private fun ViewPager2.reduceDragSensitivity(f: Int = 4) {
+        val recyclerViewField = ViewPager2::class.java.getDeclaredField("mRecyclerView")
+        recyclerViewField.isAccessible = true
+        val recyclerView = recyclerViewField.get(this) as RecyclerView
+
+        val touchSlopField = RecyclerView::class.java.getDeclaredField("mTouchSlop")
+        touchSlopField.isAccessible = true
+        val touchSlop = touchSlopField.get(recyclerView) as Int
+        touchSlopField.set(recyclerView, touchSlop*f)
     }
 
     private fun setupTabs() {
