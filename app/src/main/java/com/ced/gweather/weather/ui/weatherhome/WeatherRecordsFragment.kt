752b354f02adf233ca450af2cc15a623794ccbae
Vyron Arediano <vyron.arediano@cormanttech.com>
2022-07-11 22:16:21 +0800

    feat:  Skeleton loading in weather records screen - Added shimmer effect by FB

--- app/src/main/java/com/ced/gweather/weather/ui/weatherhome/WeatherRecordsFragment.kt
+++ app/src/main/java/com/ced/gweather/weather/ui/weatherhome/WeatherRecordsFragment.kt
@@ -1,15 +1,19 @@
 package com.ced.gweather.weather.ui.weatherhome
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ced.commons.clean.interactor.Failure
+import com.ced.commons.ui.extensions.gone
 import com.ced.commons.ui.extensions.viewModel
+import com.ced.commons.ui.extensions.visible
 import com.ced.commons.ui.observe
-import com.ced.gweather.weather.ui.BaseFragmentDI
 import com.ced.gweather.R
 import com.ced.gweather.weather.features.weatherhome.FailedToLoadWeatherRecords
 import com.ced.gweather.weather.features.weatherhome.WeatherRecordsViewModel
+import com.ced.gweather.weather.ui.BaseFragmentDI
 import com.ced.gweather_core.domain.model.WeatherModel
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.weather_records_fragment.*
@@ -44,13 +48,38 @@ class WeatherRecordsFragment : BaseFragmentDI() {
 
         weatherRecordsViewModel.loadWeatherRecords()
 
+        showShimmerEffect(true)
+
+        swipeRefreshLayoutWeatherRecords.setOnRefreshListener {
+            swipeRefreshLayoutWeatherRecords.isRefreshing = true
+            showShimmerEffect(true)
+            rvWeatherRecords.gone()
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                weatherRecordsViewModel.loadWeatherRecords()
+            }, 500)
+        }
     }
 
     private fun renderWeatherRecords(weatherRecords: List<WeatherModel>?) {
-        weatherRecords?.let { weatherRecordsAdapter.setWeatherRecords(it) }
+        swipeRefreshLayoutWeatherRecords.isRefreshing = false
+        if (weatherRecords?.size != 0) {
+            showShimmerEffect(false)
+            layoutEmptyStateWeatherRecordsView.gone()
+
+            rvWeatherRecords.visible()
+            weatherRecords?.let { weatherRecordsAdapter.setWeatherRecords(it) }
+        } else {
+            showShimmerEffect(false)
+            rvWeatherRecords.gone()
+
+            layoutEmptyStateWeatherRecordsView.visible()
+        }
     }
 
     private fun handleFailure(failure: Failure?) {
+        showShimmerEffect(false)
+
         when (failure) {
             is FailedToLoadWeatherRecords -> {
                 Snackbar.make(
@@ -60,6 +89,16 @@ class WeatherRecordsFragment : BaseFragmentDI() {
                 ).show()
             }
             else -> showFailureDialog()
+        }
+    }
+
+    private fun showShimmerEffect(show: Boolean) {
+        if (show) {
+            layoutShimmerWeatherRecords.startShimmer()
+            layoutShimmerWeatherRecords.visible()
+        } else {
+            layoutShimmerWeatherRecords.stopShimmer()
+            layoutShimmerWeatherRecords.gone()
         }
     }
 
